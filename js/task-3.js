199a27c6d7bd375bdcefb3ea75d8a78f659c87f4
tabithaambi <163632965+tabithaambi@users.noreply.github.com>
2024-03-16 07:40:29 +0000

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+    let contentWidth = parseFloat(content);
+    let paddingWidth = parseFloat(padding);
+    let borderWidth = parseFloat(border);
+    let totalSize = contentWidth + paddingWidth * 2 + borderWidth * 2;
+    return totalSize;
+}
+
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
